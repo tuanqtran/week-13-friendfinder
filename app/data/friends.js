b706fc83006926fdfece1756f3917a1fc78c091e
Tuan Tran <tuan.tran1122@gmail.com>
2016-10-28 15:44:25 -0500

    Troubleshooting files.

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [
+	{
+		name: 'Tony',
+		photo: 'https://aviary.com/img/photo-landscape.jpg',
+		score: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+	},
+	{
+		name: 'Marcus',
+		photo: 'https://static.pexels.com/photos/46710/pexels-photo-46710.jpeg',
+		score: ["3", "3", "2", "2", "1", "1", "3" ,"3" ,"2", "1"]
+	},
+	{
+		name: 'Brandon',
+		photo: 'https://static.pexels.com/photos/46710/pexels-photo-46710.jpeg',
+		score: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "4"]
+	}
+];
+
+module.exports = {friendsArray: friendsArray};
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	path = require('path');
+
+var app = express(),
+	PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use('/css', express.static(__dirname + '/app/public/css'));
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
